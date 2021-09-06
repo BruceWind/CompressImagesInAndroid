340a3791ae79baf2d91c0c8fcb79191d36bb4f9a
bruce <hide@hide.com>
2021-09-06 17:05:31 +0800

    fix: github action build error.

--- shell/install_app.sh
+++ shell/install_app.sh
@@ -11,6 +11,13 @@ command_exists() {
     fi
 }
 
+
+install_app_on_ubuntu() {
+    echo "Debian try to install $1."
+    sudo apt update
+    sudo apt install -y $1
+}
+
 install_app_on_debian() {
     echo "Debian try to install $1."
     apt update
@@ -24,7 +31,9 @@ install_app_on_mac() {
 
 install_app() {
     echo "try to install $1."
-    if [ -f /etc/debian_version ]; then
+    if cat /etc/*release | grep ^NAME | grep Ubuntu; then
+        install_app_on_ubuntu $1
+    elif [ -f /etc/debian_version ]; then
         install_app_on_debian $1
     elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
         install_app_on_debian $1
